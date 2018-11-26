476687c0d7a1bebf6882b315ae6fa82f25f12210
Ali Raza <aliraza@bu.edu>
2018-11-26 16:32:40 -0500

    updated kernel update script and added linker scripts

--- update_kernel.sh
+++ update_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if cmp -s "../linux/include/linux/ukl.h" "ukl.h"; then
+if cmp -s "../linux/include/linux/ukl.h" "kernel/ukl.h"; then
 	printf 'ukl.h already in kernel\n'
 else
 	set -x
@@ -10,7 +10,7 @@ else
 fi
 
 
-if cmp -s "../linux/scripts/link-vmlinux.sh" "link-vmlinux.sh"; then
+if cmp -s "../linux/scripts/link-vmlinux.sh" "kernel/link-vmlinux.sh"; then
 	printf 'link-vmlinux.sh already in kernel\n'
 else
 	set -x
@@ -19,21 +19,41 @@ else
 	set +x
 fi
 
-if cmp -s "../linux/init/main.c" "main.c"; then
+if cmp -s "../linux/init/main.c" "kernel/main.c"; then
 	printf 'main.c already in kernel\n'
 else
 	set -x
-	cp link-vmlinux.sh ../linux/scripts/
+	cp main.c ../linux/init/
 	rm ../linux/vmlinux || true
 	set +x
 fi
 
-if cmp -s "../linux/.config" ".config"; then
+if cmp -s "../linux/.config" "kernel/.config"; then
 	printf '.config already in kernel\n'
 else
 	set -x
-	cp link-vmlinux.sh ../linux/scripts/
+	cp .config ../linux/
 	make -C ../linux olddefconfig
+	rm ../linux/vmlinux || true
+	set +x
+fi
+
+if cmp -s "../linux/include/asm-generic/vmlinux.lds.h" "kernel/vmlinux.lds.h"; then
+	printf 'vmlinux.lds.h already in kernel\n'
+else
+	set -x
+	cp vmlinux.lds.h ../linux/include/asm-generic/
+	rm ../linux/arch/x86/kernel/vmlinux.lds || true
+	rm ../linux/vmlinux || true
+	set +x
+fi
+
+if cmp -s "../linux/arch/x86/kernel/vmlinux.lds.S" "kernel/vmlinux.lds.S"; then
+	printf 'vmlinux.lds.S already in kernel\n'
+else
+	set -x
+	cp vmlinux.lds.S ../linux/arch/x86/kernel/
+	rm ../linux/arch/x86/kernel/vmlinux.lds || true
 	rm ../linux/vmlinux || true
 	set +x
 fi
