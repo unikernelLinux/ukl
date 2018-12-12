67ddc6b2b2ed4ed22ef708d974b598992f80b8eb
Ali Raza <aliraza@bu.edu>
2018-12-12 07:58:27 -0500

    tls working for kthreads

--- multithread.c
+++ multithread.c
@@ -46,12 +46,14 @@ __thread int myCounter __attribute__ ((aligned (4096))) = 0;
 void cpuprint(int a){
 
 	loadsegment(fs, 0);
-	wrmsrl(MSR_FS_BASE, __tls_start);
+	wrmsrl(MSR_FS_BASE, __tls_start + a*sizeof(int));
+	thread[a]->thread.fsbase = __tls_start + a*sizeof(int);
+
 	int i = 0, j = 0, k = 0;
 	while(1){
 		// printk("Thread %d running on CPU no. %d.\n", a, smp_processor_id());
-		myCounter = myCounter + a;
-		printk("Thread %d: New value of myCounter = %d\n", a, myCounter);
+		myCounter = myCounter + a + 1;
+		printk("Thread %d: New value of myCounter = %d\n", a + 1, myCounter);
 		while(i < 100000000){
 			i++;
 			while(j < 100000000){
@@ -65,20 +67,7 @@ void cpuprint(int a){
 		}
 		i = 0;
 		cond_resched();
-		loadsegment(fs, 0);
-		wrmsrl(MSR_FS_BASE, __tls_start);	
 	}
-}
-
-static int get_free_idx(void)
-{
-	struct thread_struct *t = &current->thread;
-	int idx;
-
-	for (idx = 0; idx < GDT_ENTRY_TLS_ENTRIES; idx++)
-		if (desc_empty(&t->tls_array[idx]))
-			return idx + GDT_ENTRY_TLS_MIN;
-	return -ESRCH;
 }
 
 int kmain(void)
@@ -115,7 +104,7 @@ int kmain(void)
 	myCounter = myCounter + 1;
 	printk("Main Thread: New value of myCounter = %d\n", myCounter);
 	for(; i < MAX_THREADS; i++){
-		thread[i] = kthread_run((void *)cpuprint, &i, "Thread %d\n", i);
+		thread[i] = kthread_run((void *)cpuprint, i, "Thread %d\n", i+1);
 	}
 
 	while(1){
