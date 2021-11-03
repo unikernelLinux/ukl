08fa4d7acf521c7983ead50276aabe3ff7963fc3
Wiley Hunt <whunt@bu.edu>
2021-11-03 15:03:04 -0400

    added files for latency test

---
+++ tests/run_lmbench_latency.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#Name of File where output should be directed
+test_output="test.out"
+
+#String to Print Results
+RESULTS=""
+
+#Remove old object Files
+rm unit_test.o
+rm ../unit_test.o
+
+#Make test
+make lmbench_latency_test
+
+#Move Test file to UKL Directory
+cp unit_test.o ../
+
+#Navigate to UKL Directory
+cd ../
+
+
+#Build Test as UKL Application
+make unit_test
+make linux-build
+
+#Run Test in UKL
+sudo timeout 10m make run
+
+#Insert call to Python Script
+
+
+# # Successful.
+rm -f $test_output
+#make clean_test
+
+exit 0
