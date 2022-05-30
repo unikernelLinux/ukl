49e2bf0ccd09b30f843cbafbb5b227da58e9a76a
Richard W.M. Jones <rjones@redhat.com>
2022-05-30 11:21:04 +0100

    build: Don't fail in iconv subdirectory
    Previously we ran make -j10 |& tee ... which hid the failure when
    glibc builds.  In fact it builds libc.a OK (the target that we want),
    but then fails a bit later when building in the iconv/ subdirectory
    (which we don't care about for now).
    
    It turns out we cannot do the simple fix of building target libc.a,
    or at least I cannot work out what actual target to use for that.
    
    Instead, ignore errors from the make command, but add an "ls libc.a"
    (which will fail, and exit the whole build if libc.a was not built).
    
    Signed-off-by: Richard W.M. Jones <rjones@redhat.com>

--- cleanbuild.sh
+++ cleanbuild.sh
@@ -22,12 +22,10 @@ rm -rf glibc-build
 mkdir glibc-build
 cd glibc-build
 ../glibc/configure CFLAGS="-g -O2 -fthread-jumps -mcmodel=kernel -mno-red-zone ${BP_FLAG}" --prefix=/home/fedora/unikernel/build-glibc/glibc-build --enable-hacker-mode --enable-timezone-tools --disable-build-nscd --disable-nscd --disable-pt_chown --enable-static-nss x86_64-ukl --disable-shared --disable-tunables --disable-werror build_alias=x86_64-ukl host_alias=x86_64-ukl target_alias=x86_64-ukl
-make -j10
+make -j10 ||:
+ls libc.a
 make -j10 subdirs=nptl
+ls nptl/libpthread.a
 make -j10 subdirs=math
 make -j10 subdirs=rt
 cd ..
-
-ls glibc-build/libc.a
-ls glibc-build/nptl/libpthread.a
-
