e56a3e0673e41fec59b5841ed7ea7f6358bdfc8d
Richard W.M. Jones <rjones@redhat.com>
2022-05-31 19:26:03 +0100

    hello: Don't exit the program
    Signed-off-by: Richard W.M. Jones <rjones@redhat.com>

--- hello/hello-world.c
+++ hello/hello-world.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
   printf("hello, from the UKL world\n");
+  for (;;)
+    pause ();
   exit(EXIT_SUCCESS);
 }
