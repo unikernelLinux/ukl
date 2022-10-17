4c8304eaa1446adcca8009f0c0cbb54d6113652d
Eric B Munson <munsoner@bu.edu>
2022-10-17 09:46:44 -0400

    Add fio as UKL target
    We need to run a storage workload and this is a respected choice.
    
    Signed-off-by: Eric B Munson <munsoner@bu.edu>

---
+++ fio/math_hack.c
@@ -0,0 +1,9 @@
+#include <math.h>
+
+double __exp2_finite(double x) { return exp2(x); }
+double __pow_finite(double x, double y) { return pow(x, y); }
+double _ZGVdN4vv___pow_finite(double x, double y) { return pow(x, y); }
+double _ZGVdN4vv_pow(double x, double y) { return pow(x, y); }
+double __log_finite(double x) { return log(x); }
+float __logf_finite(float x) { return logf(x); }
+double __log10_finite(double x) { return log10(x); }
