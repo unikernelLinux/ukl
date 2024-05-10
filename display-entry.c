e45f0b69425189954602c93baaa9c951deb436e2
vance-raiti <vraiti77@gmail.com>
2024-05-10 17:55:42 +0000

    cleanup

--- display-entry.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-extern long entry_SYSCALL_64;
-
-int main(){
-	printf("%p",entry_SYSCALL_64);
-	return 0;
-}
--- rtld.gdb
+++
@@ -1,2 +0,0 @@
-target remote :1234
-symbol-file ld-linux-x86-64.so.2 -o 0x7ffff7fd5000
