e7a4953933531861dadf63be47483e968cc4cfb3
vance-raiti <vraiti77@gmail.com>
2024-07-07 17:18:07 +0000

    cleanup

--- initrd/create-initrd.sh
+++ initrd/create-initrd.sh
@@ -2,12 +2,15 @@
 
 set -x
 
+# create a base image with all of our desired packages
 docker build -t ukl-base .
 
 CONTAINER=init-builder-$USER
 docker stop $CONTAINER
 docker rm -f $CONTAINER
 ABSINIT=`readlink -f .`
+
+# use base image to create a proper filesystem for our initrd
 docker run --rm --privileged --name=${CONTAINER} -v ${ABSINIT}:/src -dit ukl-base /bin/bash
 docker exec -w /src/ -it $CONTAINER rm -rf ukl-initrd
 docker exec -w /src/ -it $CONTAINER ./set-passwd.sh
